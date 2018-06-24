bcd88876df5acc73c24c66298d0ea6f4b60f4f97
alxndrx <alexander@my-e-mail.net>
2018-06-24 15:34:23 +0200

    Add ReplacementTable tests with resources

---
+++ src/test/java/ReplacementTableTest.java
@@ -0,0 +1,113 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ReplacementTableTest
+{
+    private static final int EXAMPLE_COUNT = 3;
+
+    private ReplacementTable[] tables = new ReplacementTable[EXAMPLE_COUNT];
+
+    @Before
+    public void init()
+    {
+        for (int i = 0; i < EXAMPLE_COUNT; i++)
+        {
+            try
+            {
+                // Initialize tables using html downloaded by hand
+                String resourceName = String.format("example%d.html", i + 1);
+                String html = Utils.readFileToEnd(getClass().getResource(resourceName).getFile());
+                tables[i] = ReplacementTable.parseFromHtml(html);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void testGetReplacements()
+    {
+        String[][] expected = new String[][]
+                {
+                        {
+                                "22.6. | 11 | 5 - 6 | --- | --- | rev2 | Exkursion",
+                                "22.6. | 11 | 1 - 2 | E2 | 101 R | E2 | E findet statt",
+                                "22.6. | 11 | 1 - 2 | --- | --- | E5 | fällt aus"
+                        },
+                        {
+                                "25.6. | 11 | 5 - 6 | --- | --- | bio2 | fällt aus"
+                        },
+                        {
+                                "25.6. | 10A | 3 | D | 122 R | B | Tausch",
+                                "25.6. | 10A | 4 | D | 122 R | B | Tausch"
+                        }
+                };
+
+        for (int i = 0; i < expected.length; i++)
+        {
+            List<Replacement> replacements = tables[i].getReplacements();
+
+            for (int j = 0; j < replacements.size(); j++)
+            {
+                // Check whether replacement content are the same as expected
+                assertEquals(expected[i][j], replacements.get(j).toString());
+            }
+        }
+    }
+
+    @Test
+    public void testGetReplacementsWithFilter()
+    {
+        String[][] expected = new String[][]
+                {
+                        {
+                                "22.6. | 11 | 1 - 2 | E2 | 101 R | E2 | E findet statt",
+                                "22.6. | 11 | 1 - 2 | --- | --- | E5 | fällt aus"
+                        },
+                        {
+                        },
+                        {
+                                "25.6. | 10A | 4 | D | 122 R | B | Tausch"
+                        }
+                };
+
+        for (int i = 0; i < expected.length; i++)
+        {
+            // Configure a unique filter foreach replacement table
+            HashMap<ReplacementFilter, String[]> filter = new HashMap<>();
+            switch (i)
+            {
+                case 0:
+                    filter.put(ReplacementFilter.PERIOD, new String[] {"1 - 2"});
+                    filter.put(ReplacementFilter.OLD_SUBJECT, new String[] {"E"});
+                    break;
+                case 1:
+                    filter.put(ReplacementFilter.TEXT, new String[] {"fällt"});
+                    filter.put(ReplacementFilter.ROOM, new String[] {"418"});
+                    break;
+                case 2:
+                    filter.put(ReplacementFilter.ROOM, new String[] {"122"});
+                    filter.put(ReplacementFilter.SUBJECT, new String[] {"D"});
+                    filter.put(ReplacementFilter.PERIOD, new String[] {"4"});
+            }
+
+            // Check whether there are as many filtered replacements as expected
+            List<Replacement> replacements = tables[i].getReplacements(filter);
+            assertEquals(expected[i].length, replacements.size());
+
+            for (int j = 0; j < replacements.size(); j++)
+            {
+                // Check whether replacement content are the same as expected
+                assertEquals(expected[i][j], replacements.get(j).toString());
+            }
+        }
+    }
+}
